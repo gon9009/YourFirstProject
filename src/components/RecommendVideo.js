46f598634d1eb55321b792252094a7404e911290
gon <jrteab99@gmail.com>
2024-05-28 09:49:00 +0900

    feat: 추천 영상 패칭 로직 커스텀 훅으로 분리
    - 추천 영상 패칭 로직을 useFetchVideo 커스텀 훅으로 분리
    - 로딩,에러 처리 상태 추가

--- src/components/RecommendVideo.js
+++ src/components/RecommendVideo.js
@@ -1,27 +1,17 @@
 import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
-const API_KEY = process.env.REACT_APP_API_KEY;
+import useFetchVideo from "./useFetchVideo";
 
-// 유튜브 추천 화면 
+// 유튜브 추천 화면
 function RecommendVideo({ recommendProject }) {
-  const [video, setVideo] = useState(null);
+  const { video, error, loading } = useFetchVideo(recommendProject);
 
-  // 동영상 패칭
-  useEffect(() => {
-    const fetchURL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${recommendProject.searchingyoutube}&part=snippet&type=video`;
-    const fetchVideo = async () => {
-      try {
-        const response = await axios.get(fetchURL);
-        setVideo(response.data.items[0]);
-      } catch (err) {
-        console.error("동영상을 가져오는데 실패했습니다");
-      }
-    };
-    fetchVideo();
-  }, [recommendProject]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-
+  if (error) {
+    return <div>{error}</div>;
+  }
   return (
     <section className="recommend-video">
       {video && (
---
+++ src/components/useFetchVideo.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+// 추천 영상 패칭 커스텀 훅
+function useFetchVideo(recommendProject) {
+  const [video, setVideo] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchURL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${recommendProject.searchingyoutube}&part=snippet&type=video`;
+
+    const fetchVideo = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(fetchURL);
+        setVideo(response.data.items[0]);
+      } catch (error) {
+        setError("돋영상을 가져오는데 실패했습니다!");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchVideo();
+  }, [recommendProject]);
+
+  return {video,error,loading};
+}
+
+export default useFetchVideo;
