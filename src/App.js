dd2738380510954eea5f4f230819e739a6678db7
gon <jrteab99@gmail.com>
2024-05-28 12:36:48 +0900

    커밋 오류 다시시도

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MainPage from "./components/MainPage/MainPage.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout.js";
-import ResultPageContainer from "./components/ResultPage/ResultPageContainer.js.js";
+import ResultPageContainer from "./components/ResultPage/ResultPageContainer.js";
 import { ScoreProvider } from "./context/ScoreContext";
 import QuestionsContainer from "./components/Questions/QuestionsContainer.js";
 
